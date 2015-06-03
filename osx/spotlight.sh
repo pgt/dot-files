945b6f51017921320dbf0641f3f85d670561021e
Pedro Gomes Tavares <pgtnetwork@gmail.com>
2015-06-03 12:22:02 -0300

    Remove `spotlight.sh` preferences
    This was made cause when disabling spotlight from menu bar, this
    prevents the execution of Spotlight too. I don't want that.

--- osx/spotlight.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-green_echo "Spotlight: Hide Spotlight tray-icon (and subsequent helper)"
-red_echo "TODO: It's necessary more tests, cause appearently doesn't work, maybe works after reboot"
-sudo chmod 600 /System/Library/CoreServices/Search.bundle/Contents/MacOS/Search
