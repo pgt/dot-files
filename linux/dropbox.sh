14b1e09ede2d8fea591fd8cbb421f31ae53fce8d
Pedro Gomes Tavares <pgtnetwork@gmail.com>
2015-11-09 20:07:13 -0200

    Improving postgres installation

--- linux/dropbox.sh
+++ linux/dropbox.sh
@@ -4,9 +4,7 @@ __install_dropbox(){
     # Baixar o arquivo para o /tmp instalar e depois excluir o arquivo. De alguma forma verificar se foi instalado corretamente.
 
     cd ~ && wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -
-    ~/.dropbox-dist/dropboxd
-
-    return 0;
+    # TODO: Verificar se tem um arquivo no ~/.dropbox-dist/dropboxd, isso confirma que foi instalado com sucesso
 }
 
 __install_dropbox
--- linux/postgresql.sh
+++ linux/postgresql.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-# https://github.com/rranelli/linuxsetup/blob/master/scripts/pg_hba.conf
+# TODO: Ter um arquivo pg_hba.conf pré-configurado. https://github.com/rranelli/linuxsetup/blob/master/scripts/pg_hba.conf
 
-sudo sh -c "echo 'deb http://apt.postgresql.org/pub/repos/apt/ precise-pgdg main' > /etc/apt/sources.list.d/pgdg.list"
+__install_postgresql() {
+    sudo sh -c "echo 'deb http://apt.postgresql.org/pub/repos/apt/ precise-pgdg main' > /etc/apt/sources.list.d/pgdg.list"
 
-wget --quiet -O - http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc | sudo apt-key add -
+    wget --quiet -O - http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc | sudo apt-key add -
 
-__update_source_list
+    __update_source_list
 
-__install postgresql-common
-__install postgresql-9.4 libpq-dev
+    __install postgresql-common
+    __install postgresql-9.4 libpq-dev
+}
+
+__install_postgresql
