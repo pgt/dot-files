c990471d95ce81e8651b5074e753cd6a0ed7b617
Pedro Gomes Tavares <pgtnetwork@gmail.com>
2015-11-09 17:06:42 -0200

    Fix installation hh

--- linux/hh.sh
+++ linux/hh.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 __install_hh() {
-    sudo add-apt-repository ppa:ultradvorka/ppa && __install hh
+    sudo add-apt-repository ppa:ultradvorka/ppa
+    __update_source_list
+    # __install hh # TODO: I need to see how to fix this installation
 }
 
 __install_hh
