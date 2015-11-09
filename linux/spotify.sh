354c2ba978e0418216b25f367a9cb213956722ed
Pedro Gomes Tavares <pgtnetwork@gmail.com>
2015-11-09 16:12:17 -0200

    Updating the lastest spotify client package

--- linux/spotify.sh
+++ linux/spotify.sh
@@ -2,7 +2,7 @@
 
 __install_spotify(){
     local url=http://repository-origin.spotify.com/pool/non-free/s/spotify-client
-    local file=spotify-client_1.0.15.137.gbdf68615_amd64.deb
+    local file=spotify-client_1.0.17.75.g8f111100_amd64.deb
 
     wget "$url$/$file"
 
