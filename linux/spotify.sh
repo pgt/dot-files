2febdf32a7770aeadc28eb0323181271de9412b2
Pedro Gomes Tavares <pgtnetwork@gmail.com>
2015-11-09 16:18:49 -0200

    Fix spotify-client installation script

--- linux/spotify.sh
+++ linux/spotify.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 __install_spotify(){
-    local url=http://repository-origin.spotify.com/pool/non-free/s/spotify-client
-    local file=spotify-client_1.0.17.75.g8f111100_amd64.deb
+    local file="spotify-client_1.0.17.75.g8f111100_amd64.deb"
+    local url="http://repository-origin.spotify.com/pool/non-free/s/spotify-client/$file"
 
-    wget "$url$/$file"
-
-    dpkg -i "$file"
+    wget "$url"
+    sudo dpkg -i "$file"
 }
 
 __install_spotify
