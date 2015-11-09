4c381d98f936575ebe0f01570d956d108970f128
Pedro Gomes Tavares <pgtnetwork@gmail.com>
2015-11-09 16:16:02 -0200

    Refactoring chrome installation on linux

--- linux/chrome.sh
+++ linux/chrome.sh
@@ -3,13 +3,16 @@
 # TODO: Would be a nice feature check for dependencies before all
 # package installations (maybe use Makefile for this)
 __install_chrome(){
+    local file="google-chrome-stable_current_amd64.deb"
+    local url="https://dl.google.com/linux/direct/$file"
+
     # 64 bit download and install
-    if [[ -x "$(command -v gdebi)" ]]; then
-	wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-	sudo gdebi google-chrome-stable_current_amd64.deb
-    else
+    if [[ ! -x "$(command -v gdebi)" ]]; then
 	source "linux/utils.sh"
     fi
+
+    wget "$url"
+    sudo gdebi "$file"
 }
 
 __install_chrome
