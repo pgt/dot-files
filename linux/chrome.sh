bafe6c4fcb162f460cd632b7998371a3ef461e16
Pedro Gomes Tavares <pgtnetwork@gmail.com>
2015-11-09 15:09:00 -0200

    Check dependencies for chrome

--- linux/chrome.sh
+++ linux/chrome.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+# TODO: Would be a nice feature check for dependencies before all
+# package installations (maybe use Makefile for this)
 __install_chrome(){
-    # Google Chrome
     # 64 bit download and install
-    wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-    sudo gdebi google-chrome-stable_current_amd64.deb
-
-    return 0;
+    if [[ -x "$(command -v gdebi)" ]]; then
+	wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+	sudo gdebi google-chrome-stable_current_amd64.deb
+    else
+	source "utils.sh"
+    fi
 }
 
 __install_chrome
