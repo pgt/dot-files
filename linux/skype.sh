130129173189a4d6618cdad1df77a932edfa0f46
Pedro Gomes Tavares <pgtnetwork@gmail.com>
2015-11-09 16:28:02 -0200

    Add skype installation

--- linux/skype.sh
+++ linux/skype.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
 __install_skype(){
-    let url=http://download.skype.com/linux/
-    let file=skype-ubuntu-precise_4.2.0.13-1_i386.deb
+    local file="skype-ubuntu-precise_4.2.0.13-1_i386.deb"
+    local url="http://download.skype.com/linux/$file"
 
     # baixar no tmp instalar com gdebi, confirmar instalação e excluir arquivo.
-    wget -O - $url
-    sudo gdebi $file
-
-    return 0;
+    wget "$url"
+    sudo gdebi "$file"
 }
 
 __install_skype
