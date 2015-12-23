898e1523259c0a08f4cac2a6ab54af1acdee9173
Pedro Gomes Tavares <pgtnetwork@gmail.com>
2015-12-23 17:56:06 -0200

    Installing pip before installing pip packages

--- linux/install_pip_packages.sh
+++ linux/install_pip_packages.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
 __install_pip_packages() {
-    pip install pygments
+    __green_echo "Installing pip"
+    cd /tmp && wget https://bootstrap.pypa.io/get-pip.py
+    sudo python /tmp/get-pip.py
+    cd - || exit
+
+    __green_echo "Upgrading pip" && pip install -U pip
+
+    __green_echo "Installing pip packages"
+    local pip_packages=(
+	pygments
+    )
+    sudo pip install "${pip_packages[@]}"
 }
 
 __install_pip_packages
