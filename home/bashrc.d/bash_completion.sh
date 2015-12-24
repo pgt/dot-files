516b68e6349216aab784079d174f290e32c45db1
Pedro Gomes Tavares <pgtnetwork@gmail.com>
2015-12-24 10:44:53 -0200

    Improve `bash_completion`

--- home/bashrc.d/bash_completion.sh
+++ home/bashrc.d/bash_completion.sh
@@ -6,17 +6,18 @@ __bash_completion() {
 
     if [[ $platform = "linux" ]]; then
 	if [ -f /etc/bash_completion ]; then
-            . /etc/bash_completion
+            source /etc/bash_completion
 	fi
     elif [[ $platform = "macosx" ]]; then
 	if [ -f "$(brew --prefix)/etc/bash_completion" ]; then
 	    if [[ $bash_version == "4" ]]; then
-		. "$(brew --prefix)/etc/bash_completion"
+		source "$(brew --prefix)/etc/bash_completion"
 	    else
 		__red_echo "Your bash version is $bash_version. You need bash newer than 4."
 	    fi
 	fi
     else
+	__red_echo "This is not Linux or OSX, so what are you doing man?"
 	exit 1; # Let's get out of this
     fi
 
