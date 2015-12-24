97a3b5a2aadc2f50420b649b48620620d7368092
Pedro Gomes Tavares <pgtnetwork@gmail.com>
2015-12-24 16:10:59 -0200

    Remove scudcloud change default icon

--- linux/scudcloud.sh
+++ linux/scudcloud.sh
@@ -5,11 +5,6 @@ __install_scudcloud(){
     __update_source_list
     __install scudcloud
     __install hunspell-en-us
-
-    # Change default icon for the real one from Slack
-    sudo dpkg-divert --add --rename --divert /opt/scudcloud/resources/scudcloud.png.real /opt/scudcloud/resources/scudcloud.png
-    sudo cp ~/scudcloud.png /opt/scudcloud/resources/
-    sudo chmod +r /opt/scudcloud/resources/scudcloud.png
 }
 
 __install_scudcloud
