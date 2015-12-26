a288451ee9ac151276b5f447bc8f6393d17f82ea
Pedro Gomes Tavares <pgtnetwork@gmail.com>
2015-12-26 20:51:59 -0200

    [FIX] NPM Package installation

--- osx/install_npm_packages.sh
+++ osx/install_npm_packages.sh
@@ -4,6 +4,7 @@ __install_nodejs() {
     # TODO: Verificar antes se já não está instalado
     cd /tmp && wget https://nodejs.org/dist/v5.3.0/node-v5.3.0.pkg
     open /tmp/node-v5.3.0.pkg
+    cd - || exit
 }
 
 __install_npm_packages() {
