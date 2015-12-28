35ede66d5cdf87cb2ecc95306de94fbfdd5ea982
Pedro Gomes Tavares <pgtnetwork@gmail.com>
2015-12-28 18:35:49 -0200

    Implement linux golang installation

--- linux/golang.sh
+++ linux/golang.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
 __install_golang(){
-    __yellow_echo "Pending implementation of Golang installation"
+    local already_installed
+    already_installed=$(__already_installed "go")
 
-    # Create default directories for $GOPATH
-    mkdir -p "$HOME/bin"
-    mkdir -p "$HOME/pkg"
-    mkdir -p "$HOME/src"
+    if [[ $already_installed = "not_installed" ]]; then
+	cd /tmp && curl -O https://storage.googleapis.com/golang/go1.4.2.linux-amd64.tar.gz
+
+	sudo tar -C /usr/local -xzf /tmp/go1.4.2.linux-amd64.tar.gz
+
+	cd - || exit
+
+	# Create default directories for $GOPATH
+	mkdir -p "$HOME/gocode/bin"
+	mkdir -p "$HOME/gocode/pkg"
+	mkdir -p "$HOME/gocode/src"
+    else
+	__green_echo "Already installed"
+    fi
 }
 
 __install_golang
