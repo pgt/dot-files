520185699db8d231446b24a6d261fccf5c38f5b0
Pedro Gomes Tavares <pgtnetwork@gmail.com>
2015-12-28 00:35:18 -0200

    Improve `linux/dbeaver` installation

--- linux/dbeaver.sh
+++ linux/dbeaver.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
 __install_dbeaver(){
-    file="dbeaver-ce_latest_amd64.deb"
-    url="http://dbeaver.jkiss.org/files/$file"
+    local already_installed
+    already_installed=$(__already_installed "heroku")
 
-    wget "$url"
-    gdebi "$file"
+    if [[ $already_installed = "not_installed" ]]; then
+	local file="dbeaver-ce_latest_amd64.deb"
+	local url="http://dbeaver.jkiss.org/files/$file"
+
+	wget "$url" --directory-prefix=/tmp
+	sudo gdebi "/tmp/$file"
+	rm -rf "/tmp/$file"
+    else
+	__green_echo "Already installed"
+    fi
+
 }
 
 __install_dbeaver
