28bc9e1f8d4bd322dcf775dca1380865f6520262
Pedro Gomes Tavares <pgtnetwork@gmail.com>
2015-12-28 01:10:34 -0200

    Dir_colors only for linux

--- home/bashrc.d/dir_colors.sh
+++ home/bashrc.d/dir_colors.sh
@@ -2,4 +2,9 @@
 
 # To configure this I used the solarized dir theme located on this
 # link https://github.com/seebi/dircolors-solarized
-eval `dircolors $HOME/.dir_colors`
+local platform
+platform=$(__platform)
+
+if [ "$platform" = "linux" ]; then
+    eval `dircolors $HOME/.dir_colors`
+fi
