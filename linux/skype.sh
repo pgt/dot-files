5558dfeb58c22b578d4164cea4eeb676dc4ede4c
Pedro Gomes Tavares <pgtnetwork@gmail.com>
2015-12-28 01:17:17 -0200

    Improve skype installation

--- linux/skype.sh
+++ linux/skype.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
 __install_skype(){
-    # TODO: Verificar se está instalado antes de baixar.
-    local file="skype-ubuntu-precise_4.2.0.13-1_i386.deb"
-    local url="http://download.skype.com/linux/$file"
+    local already_installed
+    already_installed=$(__already_installed "heroku")
 
-    # baixar no tmp instalar com gdebi, confirmar instalação e excluir arquivo.
-    wget "$url"
-    sudo gdebi "$file"
+    if [[ $already_installed = "not_installed" ]]; then
+	local file="skype-ubuntu-precise_4.2.0.13-1_i386.deb"
+	local url="http://download.skype.com/linux/$file"
+
+	wget "$url" --directory-prefix=/tmp
+	sudo gdebi "/tmp/$file"
+	rm -rf "/tmp/$file"
+    else
+	__green_echo "Already installed"
+    fi
 }
 
 __install_skype
