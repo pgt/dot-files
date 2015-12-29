d7fbf6614745fcc8f16c0830f13e6daa18617409
Pedro Gomes Tavares <pgtnetwork@gmail.com>
2015-12-29 00:31:52 -0200

    Refactoring sourcecode font installation

--- linux/font-source_code_pro.sh
+++ linux/font-source_code_pro.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
 __install_fonts() {
-    # TODO: Mandar esses arquivos para /home e só fazer aqui o fc-cache -f -v
-    wget https://github.com/adobe-fonts/source-code-pro/archive/1.017R.zip
-    unzip 1.017R.zip
-    mkdir -p ~/.fonts
-    cp source-code-pro-1.017R/OTF/*.otf ~/.fonts/
+    # The files are already on ~/.fonts we just need to reload the
+    # fonts
     fc-cache -f -v
-    rm -rf source-code-pro-1.017R/ 1.017R.zip
 }
 
 __install_fonts
