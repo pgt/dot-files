85c3718584899d8d41facca9dcbe646912fed6e1
Pedro Gomes Tavares <pgtnetwork@gmail.com>
2018-12-04 20:58:24 -0200

    Add heroku autocompletion

---
+++ home/bashrc.d/heroku.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# heroku autocomplete setup
+HEROKU_AC_BASH_SETUP_PATH=/Users/pedrotavares/Library/Caches/heroku/autocomplete/bash_setup && test -f $HEROKU_AC_BASH_SETUP_PATH && source $HEROKU_AC_BASH_SETUP_PATH;
