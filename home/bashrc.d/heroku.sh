27d44accaaac45f7ac5d11a505a62211fa9a3f5c
Pedro Gomes Tavares <pgtnetwork@gmail.com>
2019-01-30 23:40:14 -0200

    Use HOME env instead of fixed path in `heroku.sh`

--- home/bashrc.d/heroku.sh
+++ home/bashrc.d/heroku.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # heroku autocomplete setup
-HEROKU_AC_BASH_SETUP_PATH=/Users/pedrotavares/Library/Caches/heroku/autocomplete/bash_setup && test -f $HEROKU_AC_BASH_SETUP_PATH && source $HEROKU_AC_BASH_SETUP_PATH;
+HEROKU_AC_BASH_SETUP_PATH="$HOME/Library/Caches/heroku/autocomplete/bash_setup" && test -f "$HEROKU_AC_BASH_SETUP_PATH" && source "$HEROKU_AC_BASH_SETUP_PATH";
